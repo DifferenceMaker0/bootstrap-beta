297a653024a48cd5e5d1cc11c99e7f1b0f0309cc
Difference Maker <differencemaker0@gmail.com>
2025-09-06 06:45:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Design workspace for wireframing and project design.
+
+### Goal is to wireframe a page of components that have their Ui.
+
+- #### Then make the next page designed for the messaging.
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
+import { Popover } from 'bootstrap';
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]')
+    .forEach(popover => {
+        new Popover(popover)
+    })
